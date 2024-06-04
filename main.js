c4f5db0549a976a0e859e533adac66aaba4e916d
Nithya-Kannan <77827910+Nithya-Kannan@users.noreply.github.com>
2024-06-04 14:47:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,15 +11,24 @@ recognition.onresult=function (event){
     var Content=event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML=Content;
-    speak();
+
+    if(Content=="take my selfie"){
+        speak();
+        console.log("taking selfie");
+    }
 }
 
 function speak(){
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="taking your selfie in 5 seconds";
     var utterThis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function()
+{
+    take_snapshot();
+    save();
+},5000);
 }
 
 Webcam.set({
@@ -30,3 +39,16 @@ Webcam.set({
 });
 
 camera=document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_img" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_img").src;
+    link.href=image;
+    link.click();
+}
